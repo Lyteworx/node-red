83835ebbeaf4572354f042d2933203538537907f
TJKoury <me@tjkoury.com>
2016-02-02 06:32:54 -0700

    progress on admin_server

--- red.js
+++ red.js
@@ -14,10 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-var http = require('http');
-var https = require('https');
+
 var util = require("util");
-var express = require("express");
 var crypto = require("crypto");
 try { bcrypt = require('bcrypt'); }
 catch(e) { bcrypt = require('bcryptjs'); }
@@ -27,10 +25,9 @@ var RED = require("./red/red.js");
 
 process.env.RED_VERSION = RED.version();
 
-var app = express();
 var settings = require('./lib/settings');
 var server = require('./lib/admin_server');
-var basicAuthMiddleware = require('./lib/middleware/basic_auth');
+
 
 try {
 
@@ -51,29 +48,6 @@ try {
     process.exit(1);
 }
 
-if (settings.httpAdminRoot !== false && settings.httpAdminAuth) {
-    RED.log.warn(RED.log._("server.httpadminauth-deprecated"));
-    app.use(settings.httpAdminRoot, basicAuthMiddleware(settings.httpAdminAuth.user,settings.httpAdminAuth.pass));
-}
-
-if (settings.httpAdminRoot !== false) {
-    app.use(settings.httpAdminRoot,RED.httpAdmin);
-}
-if (settings.httpNodeRoot !== false && settings.httpNodeAuth) {
-    app.use(settings.httpNodeRoot,basicAuthMiddleware(settings.httpNodeAuth.user,settings.httpNodeAuth.pass));
-}
-if (settings.httpNodeRoot !== false) {
-    app.use(settings.httpNodeRoot,RED.httpNode);
-}
-
-if (settings.httpStatic) {
-    settings.httpStaticAuth = settings.httpStaticAuth || settings.httpAuth;
-    if (settings.httpStaticAuth) {
-        app.use("/",basicAuthMiddleware(settings.httpStaticAuth.user,settings.httpStaticAuth.pass));
-    }
-    app.use("/",express.static(settings.httpStatic));
-}
-
 function getListenPath() {
     var listenPath = 'http'+(settings.https?'s':'')+'://'+
                     (settings.uiHost == '0.0.0.0'?'127.0.0.1':settings.uiHost)+
@@ -87,31 +61,7 @@ function getListenPath() {
 };
 
 RED.start().then(function() {
-    if (settings.httpAdminRoot !== false || settings.httpNodeRoot !== false || settings.httpStatic) {
-        server.on('error', function(err) {
-            if (err.errno === "EADDRINUSE") {
-                RED.log.error(RED.log._("server.unable-to-listen", {listenpath:getListenPath()}));
-                RED.log.error(RED.log._("server.port-in-use"));
-            } else {
-                RED.log.error(RED.log._("server.uncaught-exception"));
-                if (err.stack) {
-                    RED.log.error(err.stack);
-                } else {
-                    RED.log.error(err);
-                }
-            }
-            process.exit(1);
-        });
-        server.listen(settings.uiPort,settings.uiHost,function() {
-            if (settings.httpAdminRoot === false) {
-                RED.log.info(RED.log._("server.admin-ui-disabled"));
-            }
-            process.title = 'node-red';
-            RED.log.info(RED.log._("server.now-running", {listenpath:getListenPath()}));
-        });
-    } else {
-        RED.log.info(RED.log._("server.headless-mode"));
-    }
+  
 
 }).otherwise(function(err) {
     RED.log.error(RED.log._("server.failed-to-start"));
